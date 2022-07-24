000b9cbaa33f57bac96164ef95191eb3da4fa72b
Kobie Drysdale <kobiedrysdale21@gmail.com>
2022-07-24 09:33:54 +0800

    Flask/Python porject using bootstrap, SQLite3 and wtforms this is for my final project of CS50

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, flash, redirect, render_template, request, session, url_for, json
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
