000162fb5c88ec831f4ce78dcce53915d6afe526
Kobie Drysdale <kobiedrysdale21@gmail.com>
2022-07-24 19:38:30 +0800

    Contruction of registration form including some front end validation and layout

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, validators, DateField, EmailField, IntegerField, SelectField, PasswordField
 
 # Configure application
 app = Flask(__name__)
 
-class UserRegistrationForm(FORM):
+class UserRegistrationForm(Form):
     first_name = StringField("First name", validators=[validators.InputRequired()])
     uname = StringField("User name", validators=[validators.InputRequired()])
+    last_name= StringField("Last Name", validators=[validators.InputRequired()])
+    dob = DateField("DOB", validators=[validators.InputRequired()])
+    email= EmailField("Email", validators=[validators.InputRequired()])
+    height = IntegerField("Height", validators=[validators.InputRequired()])
+    weight = IntegerField("Weight", validators=[validators.InputRequired()])
+    gender = SelectField("Gender", choices=["Male", "Female", "Other", "Prefer not to answer"])
+    weight_unit = SelectField("Weight unit", choices=["kg", "lb"])
+    height_unit = SelectField("Height unit", choices=["cm", "ft/in"])
+    password = PasswordField("Password", validators=[validators.InputRequired()])
+    confirm = PasswordField("Confirm", validators=[validators.InputRequired()])
+
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
@@ -29,5 +40,8 @@ def index():
 
 
 @app.route("/register", methods=["POST", "GET"])
-def resgister():
-    return render_template("register.html")
+def register():
+    form = UserRegistrationForm(request.form)
+    return render_template("register.html", form=form)
+
+app.run(host="0.0.0.0", port=50100, debug=True)
